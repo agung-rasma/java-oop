7c458382ea770da807fb5da3d0ad2894093929a7
Agung Wijaya Rasma <agungajus14@gmail.com>
2023-05-14 21:32:44 +0800

    init git

---
+++ src/PersonApp.java
@@ -0,0 +1,23 @@
+public class PersonApp {
+    public static void main(String[] args) {
+
+        Person person1 = new Person();
+        person1.name="Agung";
+        person1.address="Marga";
+        //person1.country="Bali";
+
+        System.out.println(person1.name);
+        System.out.println(person1.address);
+        System.out.println(person1.country);
+
+        person1.sayHello("Johan");
+
+
+        Person person2 = new Person();
+        Person person3 = new Person();
+        person3.name="Wayan";
+        person3.sayHello("Mike");
+
+
+    }
+}
