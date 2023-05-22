88dcff46d801e46299c7bbdf942a5fcfb30ead3b
Agung Wijaya Rasma <agungajus14@gmail.com>
2023-05-22 08:02:17 +0800

    add product app

---
+++ src/contoh/folder/data/ProductApp.java
@@ -0,0 +1,17 @@
+package contoh.folder.data;
+
+import java.text.DecimalFormat;
+import java.text.DecimalFormatSymbols;
+import java.util.Locale;
+
+public class ProductApp {
+    public static void main(String[] args) {
+        Product product=new Product("Tesla Model 3", 1_000_000_000);
+        System.out.println(product.name);
+
+        Locale locale=new Locale("id", "ID");
+        DecimalFormatSymbols decimalFormatSymbols=new DecimalFormatSymbols(locale);
+        DecimalFormat decimalFormat=new DecimalFormat("#,###.00", decimalFormatSymbols);
+        System.out.println(decimalFormat.format(product.price));
+    }
+}
