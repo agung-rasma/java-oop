7e4c70a1034dc9c8c34d84cdeda5908361b3871f
Agung Wijaya Rasma <agungajus14@gmail.com>
2023-05-26 08:19:37 +0800

    override toString() Method

--- src/contoh/folder/data/Product.java
+++ src/contoh/folder/data/Product.java
@@ -1,5 +1,9 @@
 package contoh.folder.data;
 
+import java.text.DecimalFormat;
+import java.text.DecimalFormatSymbols;
+import java.util.Locale;
+
 public class Product {
     public String name;
     public int price;
@@ -7,5 +11,13 @@ public class Product {
     public Product(String name, int price) {
         this.name = name;
         this.price = price;
+    }
+
+    Locale locale = new Locale("id", "ID");
+    DecimalFormatSymbols decimalFormatSymbols = new DecimalFormatSymbols(locale);
+    DecimalFormat decimalFormat = new DecimalFormat("#,###.00", decimalFormatSymbols);
+
+    public String toString() {
+        return "Product Name : " + name + ", Price : " + decimalFormat.format(price);
     }
 }
--- src/contoh/folder/data/ProductApp.java
+++ src/contoh/folder/data/ProductApp.java
@@ -1,17 +1,21 @@
 package contoh.folder.data;
 
-import java.text.DecimalFormat;
-import java.text.DecimalFormatSymbols;
-import java.util.Locale;
+//import java.text.DecimalFormat;
+//import java.text.DecimalFormatSymbols;
+//import java.util.Locale;
 
 public class ProductApp {
     public static void main(String[] args) {
         Product product = new Product("Tesla Model 3", 1_000_000_000);
         System.out.println(product.name);
 
-        Locale locale = new Locale("id", "ID");
-        DecimalFormatSymbols decimalFormatSymbols = new DecimalFormatSymbols(locale);
-        DecimalFormat decimalFormat = new DecimalFormat("#,###.00", decimalFormatSymbols);
-        System.out.println(decimalFormat.format(product.price));
+//        Locale locale = new Locale("id", "ID");
+//        DecimalFormatSymbols decimalFormatSymbols = new DecimalFormatSymbols(locale);
+//        DecimalFormat decimalFormat = new DecimalFormat("#,###.00", decimalFormatSymbols);
+//        System.out.println(decimalFormat.format(product.price));
+
+        System.out.println(product.price);
+
+        System.out.println(product);
     }
 }
