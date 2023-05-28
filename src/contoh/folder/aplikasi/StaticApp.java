4b44d55626eaba346fad9240f88c9c4ecbb214d6
Agung Wijaya Rasma <agungajus14@gmail.com>
2023-05-28 22:02:19 +0800

    static import

--- src/contoh/folder/aplikasi/StaticApp.java
+++ src/contoh/folder/aplikasi/StaticApp.java
@@ -1,22 +1,24 @@
 package contoh.folder.aplikasi;
 
-import contoh.folder.data.Application;
-import contoh.folder.data.Constant;
+import static contoh.folder.data.Application.*;
+import static contoh.folder.data.Constant.*;
+
 import contoh.folder.data.Country;
-import contoh.folder.util.MathUtil;
+
+import static contoh.folder.util.MathUtil.sum;
 
 public class StaticApp {
     public static void main(String[] args) {
-        System.out.println(Constant.APPLICATION);
-        System.out.println(Constant.VERSION);
+        System.out.println(APPLICATION);
+        System.out.println(VERSION);
 
-        System.out.println(MathUtil.sum(1, 1, 1, 1, 1));
+        System.out.println(sum(1, 1, 1, 1, 1));
 
         Country.City city = new Country.City();
         city.setName("Marga");
 
         System.out.println(city.getName());
 
-        System.out.println(Application.PROCESSORS);
+        System.out.println(PROCESSORS);
     }
 }
