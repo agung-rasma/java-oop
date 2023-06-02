1115f8decc897684ea936fb4dc6cb0dbd89b12e1
Agung Wijaya Rasma <agungajus14@gmail.com>
2023-06-03 06:47:29 +0800

    members enum description

--- src/contoh/folder/aplikasi/EnumApp.java
+++ src/contoh/folder/aplikasi/EnumApp.java
@@ -11,5 +11,15 @@ public class EnumApp {
 
         System.out.println(customer.getName());
         System.out.println(customer.getLevel());
+        System.out.println(customer.getLevel().getDescription());
+
+//        String levelName = Level.VIP.name();
+//        System.out.println(levelName);
+//
+//        Level level = Level.valueOf("PREMIUM"); //Must True Value
+//        System.out.println(level);
+//
+//        Level[] levels=Level.values();
+//        System.out.println(levels);
     }
 }
--- src/contoh/folder/data/Level.java
+++ src/contoh/folder/data/Level.java
@@ -1,7 +1,18 @@
 package contoh.folder.data;
 
 public enum Level {
-    STANDAR,
-    PREMIUM,
-    VIP
+    STANDAR("Standard Level"),
+    PREMIUM("Premium Level"),
+    VIP("VIP Level");
+
+    private String description;
+
+    Level(String description) {
+        this.description = description;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
 }
